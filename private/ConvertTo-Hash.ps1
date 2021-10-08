4ea18133f2fa1c1e0cca8f7f8ab9b175a55b7fee
Jaap Brasser <noreply@jaapbrasser.com>
2021-10-08 07:40:53 +0200

    Added private function for backwards compat #9

---
+++ private/ConvertTo-Hash.ps1
@@ -0,0 +1,15 @@
+function ConvertTo-Hash {
+    param (
+        [pscustomobject] $Object
+    )
+
+    $Object.PSobject.Properties | ForEach-Object -Begin {$Hash = @{}} -Process {
+        if ($_.Value.GetType() -match 'PSCustomObject') {
+            $Hash[$_.Name] = ConvertTo-Hash $_.Value
+        } else {
+            $Hash[$_.Name] = $_.Value
+        }
+    } -End {
+        $Hash
+    }
+}
